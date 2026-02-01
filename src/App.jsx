f11e369e03d3914a36d34a48ced3ca4cbf42342d
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-01 15:43:18 +0530

    initial solved question for component Topics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-function Apple(){
-  return (
-    <h1>vansh</h1>
+function App(){
+  return(
+<div>
+      <h1>First Component</h1>
+      <h1>Age: {Sum()}</h1>
+      <Myname/>
+   
+</div>
   )
 }
-export default Apple;
+function Myname(){
+  return(
+<div>
+  <h1>Vansh</h1>
+  <h1>Verma</h1>
+ 
+</div>
+  )
+}
+function Sum(){
+  return 10+9
+}
+export default App;
